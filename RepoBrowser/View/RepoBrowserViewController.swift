9b48fecf2078e7aa4afa47ce2412927291c8698b
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-07-20 18:45:28 +0300

    add textFieldDelegate methods

--- RepoBrowser/View/RepoBrowserViewController.swift
+++ RepoBrowser/View/RepoBrowserViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class RepoBrowserViewController: UIViewController {
 
+  var gitHubApi = GitHubApi()
+  var reposStore: ReposStore!
+  
+  @IBOutlet weak var searchTextField: UITextField!
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    searchTextField.delegate = self
   }
 
 
+  @IBAction func searchButton(_ sender: Any) {
+    searchTextField.endEditing(true)
+  }
+}
+
+// MARK: - UITextFieldDelegate
+extension RepoBrowserViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    searchTextField.endEditing(true)
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    if let query = searchTextField.text {
+      var gitHubURL: URL {
+        return gitHubApi.makeGitHubURL(query: query)
+      }
+      print("\(gitHubURL)")
+    
+    }
+  }
+  
+  
 }
 
