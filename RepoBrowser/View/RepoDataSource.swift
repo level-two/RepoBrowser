283111f66706fc4dc9c705b167710a334be62c97
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-07-21 18:27:49 +0300

    add repoViewModel and collectionView dataSource

---
+++ RepoBrowser/View/RepoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  RepoDataSource.swift
+//  RepoBrowser
+//
+//  Created by malakoipechyva on 7/21/20.
+//  Copyright © 2020 malakoipechyva. All rights reserved.
+//
+
+import UIKit
+
+class RepoDataSource: NSObject, UICollectionViewDataSource {
+  
+  var repo = [RepoViewModel]()
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    repo.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let identifier = "RepoCollectionViewCell"
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+    return cell
+  }
+  
+}
