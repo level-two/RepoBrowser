5bbcb021a465bc17087cc932401e0bde764f76a4
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-07-21 20:30:33 +0300

    add collectionViewCell class

---
+++ RepoBrowser/View/RepoCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RepoCollectionViewCell.swift
+//  RepoBrowser
+//
+//  Created by malakoipechyva on 7/21/20.
+//  Copyright © 2020 malakoipechyva. All rights reserved.
+//
+
+import UIKit
+
+class RepoCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var spinner: UIActivityIndicatorView!
+  @IBOutlet weak var label: UILabel!
+  
+  
+  func updateCellImage(displaying image: UIImage?) {
+    if let imageToDisplay = image {
+      spinner.stopAnimating()
+      imageView.image = imageToDisplay
+    } else {
+      spinner.startAnimating()
+      imageView.image = nil
+      label.text = ""
+    }
+  }
+}
