88b248644dd429b19a8e7ada8c03fe7fb769c2dc
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-07-21 13:58:08 +0300

    add fetch and decode data methods

--- RepoBrowser/ViewModel/ReposStore.swift
+++ RepoBrowser/ViewModel/ReposStore.swift
@@ -10,10 +10,29 @@ import Foundation
 
 class ReposStore {
   
+  var gitHubApi = GitHubApi()
+  
   let session: URLSession = {
     let config = URLSessionConfiguration.default
     return URLSession(configuration: config)
   }()
   
- 
+  func fetchRepos(url: URL?, completion: @escaping (Result<[Repo], Error>) -> Void) {
+    if let requestUrl = url {
+      let task = session.dataTask(with: requestUrl) { (data, response, error) in
+        let result = self.processReposRequest(data: data, error: error)
+        OperationQueue.main.addOperation {
+          completion(result)
+        }
+      }
+      task.resume()
+    }
+  }
+  
+  func processReposRequest(data: Data?, error: Error?) -> Result<[Repo], Error> {
+    guard let reposData = data else {
+      return .failure(error!)
+    }
+    return gitHubApi.parseJSON(reposData: reposData)
+  }
 }
